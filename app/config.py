2ccb62353b9403b0eb9b50b821eccf423852f238
anass <anass1090@hotmail.com>
2025-09-24 13:39:12 +0200

    Added multiport cors

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5173"],
+        allow_origins=["http://localhost:5173", "http://localhost:5174"],
         allow_credentials=False,
         allow_methods=["*"],
         allow_headers=["*"],
